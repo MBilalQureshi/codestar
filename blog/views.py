0002d5a168018d7c8ac583efc055948e55e5a402
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-05-10 15:29:02 +0000

    Add a form, html, and post logic inside view. forms works now

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
 
 # Generic views are beneficial for dealing with repetitive full-stack coding tasks such as displaying database contents to a webpage.
 # It handles the most common use cases in web app development.
@@ -68,6 +69,14 @@ def post_detail(request, slug):
 
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -77,5 +86,6 @@ def post_detail(request, slug):
             "coder": "Muhammad Bilal",
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
